d82b15e3574abde0d4febc3e7d4622d670dd652d
Varun Bhandia <varunbhandia@gmail.com>
2018-05-16 18:37:37 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Showing .txt File</title>
+    <style>
+        .txtStyle
+        {
+            font: arial;
+            font-size: 14px;
+            background-color: yellow ;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="txtStyle">
+            <p><iframe src="README.txt"></iframe></p>   
+        </div>
+    </div>
+</body>
+</html>
